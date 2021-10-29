8efd66a5d750cce05cd87a844c4f5444738f5b15
ReziBisma <bismarezi@gmail.com>
2021-10-29 09:18:22 +0700

    fix login

--- Startup.cs
+++ Startup.cs
@@ -71,6 +71,7 @@ namespace TicketGo
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
